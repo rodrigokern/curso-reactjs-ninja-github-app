52b2107b3af75b5700809319df161637febad9f1
Kern <rodrigokern@gmail.com>
2019-07-14 23:40:00 -0300

    'Fazendo request dos dados' - M1#40

--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+const onKeyUpFunc = (e) => {
+    const value = e.target.value
+    const keyCode = e.which || e.keyCode
+    const ENTER = 13
+    if (keyCode === ENTER) {
+        const url = `https://api.github.com/users/${value}`
+        fetch(url)
+            .then((result) => result.json())
+            .then((result) => {
+                console.log(result)
+            })
+    }
+}
+
 const Search = () => (
     <div className='search'>
         <input
             type='search'
             placeholder='Digite o nome do usuário no GitHub'
+            onKeyUp={onKeyUpFunc}
         ></input>
     </div>
 )
